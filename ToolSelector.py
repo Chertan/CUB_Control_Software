518a91af8c7e129cd86c9c62091e211d875a7b06
Chertan <ryan.breuer192@gmail.com>
2020-09-17 23:21:34 +0800

    Added tool rotation algorithm

--- ToolSelector.py
+++ ToolSelector.py
@@ -24,7 +24,7 @@ class ToolSelector:
     NEG_DIR = 0
 
     # Number of motor steps between each face of the tool
-    STEPS_PER_TOOL = 8
+    STEPS_PER_TOOL = 6
 
     # GPIO pin of the tool selector home sensor
     TRAVPS_PIN = 4
@@ -51,12 +51,31 @@ class ToolSelector:
     def tool_select(self, tool):
         # Rotate to a specific tool: (Top, Middle, Bot)
         #   0 - Blank
-        #   1 -
-        #   2 -
-        #   3 -
-        #   4 -
-        #   5 -
-        #   6 -
-        #   7 -
+        #   1 - Top
+        #   2 - Mid
+        #   3 - Top + Mid
+        #   4 - Bot
+        #   5 - Top + Bot
+        #   6 - Mid + Bot
+        #   7 - Top + Mid + Bot
 
+        # Movement is found by translating the current tool to zero
+        movement = tool - self.currentTool
+        direction = ToolSelector.POS_DIR
+
+        if movement < 0:
+            if movement < -3:
+                # Change rotation direction
+                direction = ToolSelector.NEG_DIR
+            else:
+                # Direction is +ve by default
+                movement += 8
+
+        steps = movement * ToolSelector.STEPS_PER_TOOL
+
+        # Move the Stepper motor
+        self.toolStepper.move_steps(steps, direction)
+
+        # Update the current tool attribute
+        self.currentTool = tool
 
