bb88031c15df6e8ec23853a12841ad9c4ad868e4
Chertan <ryan.breuer192@gmail.com>
2020-10-08 23:15:34 +0800

    Started Feeder abstraction class

--- Feeder.py
+++ Feeder.py
@@ -13,6 +13,89 @@
 import pigpio
 import time
 import logging
+from StepperMotor import StepperMotor
+from DCOutputDevice import DCOutputDevice
+from PhotoSensor import PhotoSensor
+
 
 class Feeder:
+
+    # // Line Feed Stepper Motor //
+    # GPIO pins of the line feed stepper motor
+    LNFDIR = 24
+    LNFSTEP = 18
+    LNFENA = 23
+    # Motor speed parameters to be tuned during testing
+    START_SPEED = 5
+    MAX_SPEED = 10
+    # Motor directional parameters
+    LNF_POS_DIR = 1
+    LNF_NEG_DIR = 0
+
+    # // Paper Feed DC Motor //
+    # GPIO pins of the paper feed DC motor
+    PPRDIR = 25
+    PPRENA = 12
+    # Motor Directional parameters
+    PPR_POS_DIR = 1
+    PPR_NEG_DIR = 0
+
+    # // Input Side Paper Sensor //
+    # GPIO pin of the input side paper sensor
+    PBOTPS = 22
+    # GPIO Input for the sensor to return True
+    PBOTPS_TRUE = 0
+
+    # // Output Side Paper Sensor //
+    # GPIO pin of the output side paper sensor
+    PTOPPS = 17
+    # GPIO Input for the sensor to return True
+    PBTOPPS_TRUE = 0
+
+    # // A4 Paper Size Sensor //
+    # GPIO pin of the A4 size paper sensor
+    A4PPRPS = 14
+    # GPIO Input for the sensor to return True
+    A4PPRPS_TRUE = 0
+
+    # // Braille Paper Size Sensor //
+    # GPIO pin of the A4 size paper sensor
+    BPPRPS = 15
+    # GPIO Input for the sensor to return True
+    BPPRPS_TRUE = 0
+
+    def __init__(self):
+        # Construct Line Stepper
+        self.LineStepper = StepperMotor(Feeder.LNFDIR, Feeder.LNFSTEP, Feeder.LNFENA,
+                                         Feeder.START_SPEED, Feeder.MAX_SPEED)
+        logging.info(f"Setting up Line Feed Stepper with STEP Pin: {Feeder.LNFSTEP}")
+
+        # Construct Paper Feed
+        self.PaperFeed = DCOutputDevice(Feeder.PPRDIR, Feeder.PPRENA)
+        logging.info(f"Setting up Paper Feed Motor with ENA Pin: {Feeder.PPRENA}")
+
+        self.LineInputSensor = PhotoSensor(Feeder.PBOTPS, Feeder.PBOTPS_TRUE)
+        logging.info(f"Setting up Paper Input Sensor on Pin: {Feeder.PBOTPS}")
+
+        self.LineInputSensor = PhotoSensor(Feeder.PTOPPS, Feeder.PBTOPPS_TRUE)
+        logging.info(f"Setting up Paper Output Sensor on Pin: {Feeder.PTOPPS}")
+
+        self.LineInputSensor = PhotoSensor(Feeder.A4PPRPS, Feeder.A4PPRPS_TRUE)
+        logging.info(f"Setting up A4 Paper Size Sensor on Pin: {Feeder.A4PPRPS}")
+
+        self.LineInputSensor = PhotoSensor(Feeder.BPPRPS, Feeder.BPPRPS_TRUE)
+        logging.info(f"Setting up Braille Paper Size Sensor on Pin: {Feeder.BPPRPS}")
+
+    # Returns the size of the paper in number of characters
+    # Note that
+    def get_paper_size(self):
+
+
+    def feed_line(self):
+
+
+    def feed_lines(self, count):
+        for i in range(count):
+            self.feed_line()
+
 
