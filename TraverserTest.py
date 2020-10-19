71f68c91df71c871c094a6f22d49fd033abe52ee
Chertan <ryan.breuer192@gmail.com>
2020-10-19 13:08:28 +0800

    Simple traverser testing code

--- TraverserTest.py
+++ TraverserTest.py
@@ -9,3 +9,33 @@
 #
 #
 #
+
+from HeadTraverser import HeadTraverser
+
+
+def main():
+    traverser = HeadTraverser()
+
+    pos = 0
+
+    while True:
+
+        d = input('Enter p or n for direction')
+        n = input('Enter number of character spaces')
+
+        for i in range(0, n):
+            if pos == 30:
+                break
+            if d is 'p':
+                traverser.traverse_column()
+                pos += 1
+            if d is 'n':
+                traverser.traverse_column(reverse=True)
+                pos -= 1
+
+        if pos == 30:
+            print('Reached page max distance')
+
+
+if __name__ == "__main__":
+    main()
