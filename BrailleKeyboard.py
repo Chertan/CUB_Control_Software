d0c1425016319c55aa852a2a96c5cbd9973f480c
Chertan <ryan.breuer192@gmail.com>
2020-11-04 01:27:42 +0800

    Fixed test file to work

--- BrailleKeyboard.py
+++ BrailleKeyboard.py
@@ -1,14 +1,14 @@
 import time
 
 
-def main(out_pipe=None, in_pipe=None):
+def main(pipe=None):
     # Perform Simulated input
 
-    test_input = ['<uppercase>', 't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't', '.']
+    test_input = ['<uppercase>', 't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't', '.', "END OF INPUT"]
     i = 0
 
-    while not in_pipe.poll():
-        out_pipe.send(test_input[i])
+    while not pipe.poll():
+        pipe.send(test_input[i])
         i += 1
         time.sleep(0.5)
 
