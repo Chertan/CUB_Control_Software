597630132e56f35c12806879433362afd558350d
Chertan <ryan.breuer192@gmail.com>
2020-11-08 16:41:17 +0800

    Added comments to match documentation style and made slight adjustment to input logging method

--- component_control/Input.py
+++ component_control/Input.py
@@ -16,36 +16,79 @@ FILE_LANGUAGES = ["ENG", "UEB", "BKB"]
 class Input:
     """Functional Class to represent input into the CUB Control system
 
-        Attributes:
+        Attributes: mode           - Mode of input selected for operation
+                    runFlag        - Flag to pause and start input operation
+                    exit           - Flag to signal the input thread to finish input
+                    cub_pipe       - Pipe used by the CUB thread to communicate to the Input thread
+                    input_cub_pipe - Pipe used by the Input thread to communicate to the CUB thread
+                    BKeyboard_pipe - Pipe used by the Braille Keyboard process to communicate to the Input thread
+                    input_pipe_BKB - Pipe used by the Input thread to communicate to the Braille Keyboard process
+                    p_BKeyboard    - Running process of the Braille keyboard input program
+                    f_stdin        - File of the systems standard input
+                    terminal_old   - Settings of the terminal before input mode was entered
+                    inFilename     - File name of the input file for FILE input mode
+                    inFileLang     - Language/Format of the input file (Allows for pre-translated Braille text files)
+                    inFile         - File opened for file input
+                    inputlogFilename        - File name of the logging file for the input characters
+                    inputlogFile            - File opened for logging input characters
+                    translation_logFilename - File name of the logging file for the translated input characters
+                    translationlogFile      - File opened for logging translated input characters
 
-        Methods:
+        Methods:    thread_in()   -
+                    start_input() - Sets the run Flag to true to enable the operation of the Input thread
+                    pause_input() - Sets the run Flag to false to pause the operation of the Input thread
+                    close()       - Sets the exit flag to true to signal for the Input thread to close
+                    send(<msg>)   - Sends a message to the input thread
+                    recv()        - Receives the oldest sent message from the input thread
     """
 
-    def __init__(self, input_mode="KEYBOARD", filename="", file_language="ENG", inputlog="cub_input_log.txt", translation_log="cub_translation_log.txt"):
+    def __init__(self, input_mode="KEYBOARD", filename="", file_language="ENG", inputlog="cub_input_log.txt",
+                 translation_log="cub_translation_log.txt"):
         """Creates an abstraction object of the Embosser module for the CUB
 
+        :param input_mode: Parameter to select the input mode to be used for input
         :param filename: Optional parameter to define the input file to read the characters from
+        :param file_language: Optional parameter to select the language/format for the input file (default is English)
+        :param inputlog: Optional parameter to select the logging file for the read input
+        :param translation_log: Optional parameter to select the logging file for the translated input
         """
         logging.info(f"Setting up Input in mode: {input_mode}")
+        # Input mode to be ran, one of the items in INPUT_MODES
         self.mode = input_mode
-
+        # Constructs the running thread to false, input waits until told to start taking input
         self.runFlag = threading.Event()
-
+        # Sets exit flag to false, setting to true makes thread complete operation
         self.exit = False
+
+        # Communication pipes:
+        #   cub_pipe is the CUBs pipe for communication with the input thread
+        #   BKeyboard_pipe is the Braille Keyboards pip for communication with the input thread
+        #   input_pipe_cub and input_pipe_BKeyboard is the Input thread pip for communication with the CUB and
+        #   Braille keyboard threads respectively
         self.cub_pipe, self.input_pipe_cub = mp.Pipe()
         self.BKeyboard_pipe, self.input_pipe_BKeyboard = mp.Pipe()
-
-        self.inFilename = filename
-        self.inFileLang = file_language
-        self.inFile = None
+        # Braille keyboard process
         self.p_BKeyboard = None
 
+        # File name for the Input File
+        self.inFilename = filename
+        # Format/Language of the Input file
+        self.inFileLang = file_language
+        # File attribute to store link to input file
+        self.inFile = None
+
+        # File name for the input logging file
         self.inputlogFilename = inputlog
+        # Link to the input logging file
         self.inputlogFile = None
+        # File name for the translated input logging file
         self.translation_logFilename = translation_log
+        # Link to the translated input logging file
         self.translationlogFile = None
 
+        # Get a pointer to the standard in system file
         self.f_stdin = sys.stdin.fileno()
+        # Save a copy of terminal settings to be restored once input is complete
         self.terminal_old = termios.tcgetattr(self.f_stdin)
 
     def thread_in(self):
@@ -54,21 +97,25 @@ class Input:
         :return: None
         """
         try:
-            logging.info("Input thread Started")
-            self.startup()
-            self.run()
+            logging.debug("Input thread Started")
+            # Run component startup procedure
+            self.__startup()
+            # Run component loop
+            self.__run()
 
         except InitialisationError as err:
             self.close()
-            logging.error(f"{err.component} ERROR: {err.message}")
+            self.__output_cub(f"{err.component} ERROR: {err.message}")
         except CUBClose as close:
-            logging.warning(f"Close Signalled from {close.exit_point} - {close.message}")
+            self.__output_cub(f"Close Signalled from {close.exit_point} - {close.message}")
         except CommunicationError as comm:
             self.close()
-            logging.error(f"{comm.component} ERROR: {comm.message} - MSG: {comm.errorInput}")
+            self.__output_cub(f"{comm.component} ERROR: {comm.message} - MSG: {comm.errorInput}")
         except OperationError as op:
             self.close()
-            logging.error(f"{op.component} ERROR: {op.message} - OP: {op.operation}")
+            self.__output_cub(f"{op.component} ERROR: {op.message} - OP: {op.operation}")
+        except Exception as ex:
+            self.__output_cub(f"Undefined Input ERROR: {ex}")
         finally:
             # Clean up
             if self.inFile is not None:
@@ -80,10 +127,10 @@ class Input:
 
             # Close Braille Keyboard process if still alive
             if self.p_BKeyboard is not None and self.p_BKeyboard.is_alive():
-                self.input_pipe_BKeyboard.send("END OF INPUT")
+                self.input_pipe_BKeyboard.send("CLOSE")
                 self.p_BKeyboard.join(timeout=2)
 
-    def startup(self):
+    def __startup(self):
         """Initialises Input functions depending on input mode
 
         :return:
@@ -127,8 +174,11 @@ class Input:
             raise InitialisationError("CUBInput", f"Invalid Input Mode - {self.mode}")
         self.__output_cub("ACK")
 
-    def run(self):
+    def __run(self):
+        # Wait until main thread signals to begin input
         self.runFlag.wait()
+
+        # Check if
         if not self.exit:
             if self.mode == "KEYBOARD":
                 self.inputlogFile.write("Reading Input from Keyboard\n")
@@ -145,88 +195,115 @@ class Input:
 
             logging.info("Starting Input Loop")
 
+        # Loop until exit flag is set
         while not self.exit:
             # Get input as a list of characters in braille cell notation
-            logging.info("Taking Input from source")
-            in_chars = self.take_input()
-            logging.info(f"Input is: {in_chars}")
+            in_chars = self.__take_input()
+            logging.info(f"Input from {self.mode} is: {in_chars}")
 
             if in_chars[0] == "END OF INPUT":
-                logging.info("Input denotes end of input")
+                # Close Input, outputs end of input at close
                 self.exit = True
             else:
                 # Output each character to the Control System
-                logging.info(f"Sending input to CUB - Input: {in_chars}")
                 for char in in_chars:
-                    logging.info(f"Sending Character to CUB - Char: {char}")
                     self.__output_cub(char)
 
-            if self.mode == "FILE":
-                print(".",end='')
-                sys.stdout.flush()
             # Pause if flag is not set
             self.runFlag.wait()
 
-    def take_input(self):
+    def __take_input(self):
+        """Takes the next input from the current input mode and returns it translated into CUB Braille Format
+
+        :return: list of characters in CUB Braille format
+        """
+        # Setup return list
+        # Note: Must be list as calls to translate can convert a single character into
+        #       multiple braille characters i.e capital A is capital prefix followed by a
         chars = []
 
         # ---------
         # Keyboard
         # ---------
         if self.mode == "KEYBOARD":
-            logging.info("Retrieving Keyboard Input")
+            # Read input from keyboard via stdin
             char_raw = sys.stdin.read(1)
+            # Write the input character to log file
             self.inputlogFile.write(char_raw)
-            logging.info(f"Input retreived as : {char_raw}")
+            logging.debug(f"Input retreived from Keyboard as : {char_raw}")
+
             if char_raw == '\x03' or char_raw == '\x1a' or char_raw == '^C' or char_raw == '^Z' or char_raw == '\x1b':
+                # The input key was an exit key/combination
                 logging.info("Keyboard triggered Shutdown")
                 raise CUBClose("Keyboard Input", "Keyboard Interrupt received")
-
+            # Translate the input into CUB Braille format (Language set for english keyboard)
             chars = translate(char_raw, "ENG")
-            for character in chars:
-                self.translationlogFile.write(character + " ")
+            self.__log_input(chars)
 
         # -----------------
         # Braille Keyboard
         # -----------------
         elif self.mode == "BKEYBOARD":
-            logging.info("Retrieving Braille Keyboard input")
+            # Retrieve input from the braille keyboard pipe
             msg = self.__input_b_keyboard()
+            logging.debug(f"Input retreived from Braille Keyboard as : {msg}")
             if msg == "END OF INPUT":
-                logging.warning("Input Finished while reading")
+                # Input signals end of input
                 chars = [msg]
+                logging.info("Input Finished while reading")
             else:
                 self.inputlogFile.write(msg)
+                # Translate the input to CUB Braille format
                 chars = translate(msg, "BKB")
-                for character in chars:
-                    self.translationlogFile.write(character+ " ")
+                self.__log_input(chars)
 
         # -----------
         # File Input
         # -----------
         elif self.mode == "FILE":
             line = self.inFile.readline()
+            logging.debug(f"Input retreived from File as : {line}")
             if line == "":
+                # Input signals end of input
                 chars = ["END OF INPUT"]
-                logging.warning("Input Finished while reading")
+                logging.info("Input Finished while reading")
             else:
                 self.inputlogFile.write(line)
+                # Translate the input to CUB Braille format, grade two for full contractions
                 chars = translate(line, self.inFileLang, grade=2)
-                for character in chars:
-                    self.translationlogFile.write(character+ " ")
+                self.__log_input(chars)
+
         return chars
 
-    def logInput(chars):
+    def __log_input(self, chars):
+        """Logs the translated input characters into the translation log file
+
+        :param chars: Input characters to be logged
+        :return:
+        """
+        # Write all translated characters to the log file
         for character in chars:
-            self.translationlogFile.write(character)
+            self.translationlogFile.write(character + " ")
 
     def pause_input(self):
+        """Sets the run Flag to false to pause the operation of the Input thread
+
+        :return: None
+        """
         self.runFlag.clear()
 
     def start_input(self):
+        """Sets the run Flag to true to enable the operation of the Input thread
+
+        :return: None
+        """
         self.runFlag.set()
 
     def close(self):
+        """Sets the exit flag to true to signal for the Input thread to close
+
+        :return: None
+        """
         self.exit = True
         self.runFlag.set()
 
@@ -236,7 +313,7 @@ class Input:
         :param msg: Message to be output to another thread
         :return: None
         """
-        logging.info(f"CUBInput Sent message to CUB - {msg}")
+        logging.debug(f"CUBInput Sent message to CUB - {msg}")
         self.input_pipe_cub.send(msg)
 
     def __input_cub(self):
@@ -245,11 +322,11 @@ class Input:
         :return: The object received from another thread
         """
         msg = self.input_pipe_cub.recv()
-        logging.info(f" CUBInput Received message from CUB - {msg}")
+        logging.debug(f" CUBInput Received message from CUB - {msg}")
         return msg
 
     def send(self, msg):
-        """Used by other threads to send an object to the input pipe
+        """Used by other threads to send a message to the input thread
 
         :param msg: Message to be input to the Head Traverser Thread
         :return: None
@@ -257,12 +334,17 @@ class Input:
         self.cub_pipe.send(msg)
 
     def recv(self):
-        """Used by other threads to send an object to the input pipe
+        """Used by other threads to receive a message from the input thread
 
         :return: Message output by Head Traverser
         """
         msg = self.cub_pipe.recv()
-        logging.info(f"Cub received message from input - {msg}")
+        logging.debug(f"Cub received message from input - {msg}")
+
+        # Print to screen as progress indicator of file printing
+        if self.mode == "FILE":
+            print(".", end='')
+
         return msg
 
     def __input_b_keyboard(self):
